06a4d08a892fad521109ea9b804b5208c238ba80
Павел <Pavel@mail.ru>
2021-01-04 13:42:11 +0300

    test and doc all proekt

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-var gulp = require('gulp'),
+const  gulp = require('gulp'),
     sassdoc = require('sassdoc');
+    sass = require('gulp-sass');
+    sourcemaps = require('gulp-sourcemaps');
+    watch = require('gulp-watch');
+    minifyCSS = require('gulp-minify-css');
+    concat = require('gulp-concat');
+    gcmq = require('gulp-group-css-media-queries');
 
-  var sassdocOptions = {
+var sassdocOptions = {
       // dest: './sassdoc',
       // theme: './node_modules/sassdoc-theme-flippant'
   };
@@ -20,3 +26,23 @@ gulp.task('sassdoc', function () {
   return gulp.src(paths.sass)
     .pipe(sassdoc());
 });
+
+gulp.task('sass-compile', function () {
+  return gulp.src('test/sass/**/*.sass')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write('./test'))
+      .pipe(concat('main.css'))
+      .pipe(gulp.dest('test/css/')
+      .pipe(gcmq))
+})
+
+gulp.task('watch', function () {
+  gulp.watch('test/sass/**/*.sass', gulp.series('sass-compile'))
+})
+
+gulp.task('css', function () {
+    return gulp.src('test/css/main.css')
+        .pipe(gcmq())
+        .pipe(gulp.dest('test/css/'))
+})
